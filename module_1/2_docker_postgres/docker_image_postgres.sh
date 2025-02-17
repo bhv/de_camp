6324e5d06e156b48022e88b8d925cf6dd22f77b3
Sagar Bhavsar <sagar@basking.io>
2025-02-17 23:31:24 +0100

    dlt

--- module_1/2_docker_postgres/docker_image_postgres.sh
+++ module_1/2_docker_postgres/docker_image_postgres.sh
@@ -4,6 +4,6 @@ docker run -it \
 -e POSTGRES_USER="sagar" \
 -e POSTGRES_PASSWORD="sagar" \
 -e POSTGRES_DB="ny_taxi" \
--v /Users/sagarbhavsar/MyRepo/de_camp/module_1/docker_postgres/ny_taxi_postgres_data:/var/lib/postgresql/data \
+-v /Users/sagarbhavsar/MyRepo/de_camp/module_1/2_docker_postgres/ny_taxi_postgres_data:/var/lib/postgresql/data \
 -p 5432:5432 \
 postgres:13
